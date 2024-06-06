a94b7c838765c0098f59f0fde95bf22f732492bf
hajira-web <hajira6789@gmail.com>
2024-06-06 10:17:04 +0530

    acess modifier

---
+++ Mavensample/src/main/java/acessmodifier/acessmodifier2.java
@@ -0,0 +1,15 @@
+package acessmodifier;
+
+public class acessmodifier2 {
+
+	public static void main(String[] args) {
+		
+	Sample b=new Sample();
+	b.sampledisplay();
+	//b.sampledisplay1();
+	b.sampledisplay2();
+	b.sampledisplay3();
+
+	}
+
+}
